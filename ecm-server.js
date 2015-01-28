4420ac38e97ffb5b2e9cbb99839a84368c532c13
Gabdarab <gabordarvasi@dhcp-10-176-127-180.dynamic.eduroam.mwn.de>
2015-01-28 19:17:42 +0100

    28.01 Arbeit

--- ecm-server.js
+++ ecm-server.js
@@ -9,36 +9,30 @@ var pool = mysql.createPool({
   	database : 'ecmmobile'
 });
 
-// only for testing
-var user ={
-	"vorname" : "Stefan",
-	"nachname" : "Servermann",
-	"regcode" : "Servercode"
-};
-//
-
 var server = http.createServer(function (req, res) {
+  	
   	var parsedUrl = url.parse(req.url, true);
   	var responseString = "";
+  	
   	// post user information upon registration
 	if(parsedUrl.pathname === "/postregistration"){
+	  	
 	  	req.on('data', function (data){
 	  		responseString += data;
 	  	});
+	  	
 	  	req.on('end', function(){
 	  		var responseObject = JSON.parse(responseString);
 	  		console.log("postregistraion request recieved.");
-
+	  		
 	  		pool.getConnection(function(err, connection){
+        		
         		if (err){
             		throw err;
         		}else{
         			//include uuid and other device info for smartphones
-            		connection.query("INSERT INTO user VALUES ('tempuuid'," + responseObject.vorname + 
-            						 "," + responseObject.nachname + "," + responseObject.regcode + ","
-            						// + responseObject.devicemodel + "," + responseObject.deviceplatform + "," + responseObject.deviceversion + "," 
-            						   + "devicemodel" + "," + "deviceplatform" + "," + "deviceversion" + "," 
-            						 + responseObject.timestamp + ")", function(err, rows){
+            		connection.query("INSERT INTO user VALUES ('tempuuid', '" + responseObject.vorname + "', '" + responseObject.nachname + "', '" + responseObject.regcode + "', " + "'devicemodel', 'deviceplatform', 'deviceversion', " + responseObject.timestamp + ")", function(err, rows){
+                		
                 		if (err) {
                     		throw err;
                 		}else{
@@ -47,25 +41,31 @@ var server = http.createServer(function (req, res) {
 	    									'Access-Control-Allow-Origin': '*',
 	    									'Access-Control-Allow-Headers' : 'Content-Type'
 	    					});
+	    					
 	    					res.end(JSON.stringify({"response":"postregistration_success"}));
                 		}
             		})
+            		
             		connection.release(console.log("postregistration connection released!"));
         		};
-    		});	
+    		});
+    			
 	    });
-
 	// post user location
 	}else if (parsedUrl.pathname === "/postlocation") {
+		
 		req.on('data', function (data){
 	  		responseString += data;
 	  	});
+	  	
 	  	req.on('end', function(){
+	  		
 	  		var responseObject = JSON.parse(responseString);
-	  		console.log("postlocation request recieved.");
+	  		console.log("'postlocation' request recieved.");
 	  		//var dateToday = new Date(responseObject.timestamp);
 	  		
 	  		pool.getConnection(function(err, connection){
+        		
         		if (err){
             		throw err;
         		}else{
@@ -76,38 +76,59 @@ var server = http.createServer(function (req, res) {
                 		if (err) {
                     		throw err;
                 		}else{
-                    		console.log("postlocation SQL Query finished.");
+                    		console.log("'postlocation' SQL Query finished.");
                     		res.writeHead(200, { 'Content-Type': 'application/json', 
 	    						'Access-Control-Allow-Origin': '*',
 	    						'Access-Control-Allow-Headers' : 'Content-Type'
 	    					});
+	    					
 	    					res.end(JSON.stringify({"response" : "postlocation_success"}));
                 		}
             		})
-            		connection.release(console.log("postlocation connection released!"));
+            		
+            		connection.release(console.log("'postlocation' connection released!"));
         		};
     		});
 	    });
 
 	// get info for registered users
 	}else if (parsedUrl.pathname === "/getregistered"){
-	   	res.writeHead(200, { 'Content-Type': 'application/json', 
-    		'Access-Control-Allow-Origin': '*',
-    		'Access-Control-Allow-Headers' : 'Content-Type'
-	    });
-	    res.end(JSON.stringify(user));
+	   	
+	   	console.log("'getregistered' request recieved.");
+
+	   		pool.getConnection(function(err, connection){
+        		if (err){
+            		throw err;
+        		}else{
+            		
+            		connection.query("SELECT vorname, nachname, regcode FROM user WHERE uuid ='" + parsedUrl.query.uuid + "'", function(err, rows){
+                		if (err) {
+                    		throw err;
+                		}else{
+                    		console.log("'getregistered' SQL Query finished.");
+                    		res.writeHead(200, { 'Content-Type': 'application/json', 
+    							'Access-Control-Allow-Origin': '*',
+    							'Access-Control-Allow-Headers' : 'Content-Type'
+	    					});
+	    					res.end(JSON.stringify(rows), console.log(rows));
+                		}
+            		})
+            		
+            		connection.release(console.log("'getregistered' connection released!"));
+        		};
+    		});
 	
 	// get contacts location data
 	}else if (parsedUrl.pathname === "/getcontacts"){
-	   	//req.on('end',function(){
-
+	   	
 	   		console.log("'getcontacts' request recieved.");
 
 	   		pool.getConnection(function(err, connection){
         		if (err){
             		throw err;
         		}else{
-            		connection.query("SELECT s1.vorname, s1.nachname, s2.latitude, s2.longitude, s2.timestamp from user s1 inner join (select uuid, latitude, longitude, max(timestamp) as timestamp from location group by uuid) as s2 on s1.uuid=s2.uuid where s1.uuid <> 'myexampleuuid' and s1.regcode = 'codeSQL'", function(err, rows){
+            		
+            		connection.query("SELECT s1.vorname, s1.nachname, s2.latitude, s2.longitude, s2.timestamp FROM user s1 INNER JOIN (SELECT uuid, latitude, longitude, max(timestamp) AS timestamp FROM location GROUP BY uuid) AS s2 ON s1.uuid=s2.uuid WHERE s1.uuid <> 'myexampleuuid' AND s1.regcode = '" + parsedUrl.query.regcode + "'", function(err, rows){
                 		if (err) {
                     		throw err;
                 		}else{
@@ -119,17 +140,21 @@ var server = http.createServer(function (req, res) {
 	    					res.end(JSON.stringify(rows));
                 		}
             		})
+            		
             		connection.release(console.log("'getcontacts' connection released!"));
         		};
     		});
-	   	//})
+	   	
 	//api not found
-	} else {
+	}else{
+		
 		res.writeHead(404, { 'Content-Type': 'application/json', 
 	    	'Access-Control-Allow-Origin': '*',
 	    	'Access-Control-Allow-Headers' : 'Content-Type'
 	    });
+	    
 	    res.end();
+
 	}
 });
 
